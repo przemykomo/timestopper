e1d3125fd625ccef6893b0e8a38f26b9cf5f6b24
Przemek <przemek@przemyk.xyz>
2020-08-04 18:39:14 +0200

    fixed world loading freeze and invisible player

--- src/main/java/xyz/przemyk/timestopper/TimeStopperEvents.java
+++ src/main/java/xyz/przemyk/timestopper/TimeStopperEvents.java
@@ -1,6 +1,8 @@
 package xyz.przemyk.timestopper;
 
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import xyz.przemyk.timestopper.entities.ThrownTimeStopperEntity;
@@ -12,6 +14,9 @@ public class TimeStopperEvents {
 
     @SubscribeEvent
     public static void onEntityEvent(EntityEvent event) {
+        if (event instanceof EntityJoinWorldEvent || event instanceof RenderPlayerEvent) {
+            return;
+        }
         if (event.isCancelable() && event.getEntity() != null && !event.getEntity().world.getEntitiesWithinAABB(ThrownTimeStopperEntity.class, scan.offset(event.getEntity().getPositionVec())).isEmpty()) {
             event.setCanceled(true);
         }
