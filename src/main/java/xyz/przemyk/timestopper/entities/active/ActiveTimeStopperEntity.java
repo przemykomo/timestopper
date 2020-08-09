0a06902ae43235f7935d656f3d1d183232e6980e
Przemek <przemek@przemyk.xyz>
2020-08-09 10:04:44 +0200

    change particles

--- src/main/java/xyz/przemyk/timestopper/entities/active/ActiveTimeStopperEntity.java
+++ src/main/java/xyz/przemyk/timestopper/entities/active/ActiveTimeStopperEntity.java
@@ -61,7 +61,7 @@ public class ActiveTimeStopperEntity extends Entity {
         if (ticksLeft > 0) {
             dataManager.set(TICKS_LEFT, ticksLeft - 1);
             for (int i = 0; i < 20; ++i) {
-                world.addParticle(ParticleTypes.ENCHANT, getPosX() + (rand.nextDouble() - 0.5) * TIME_FIELD_SIZE, getPosY() + (rand.nextDouble() - 0.5) * TIME_FIELD_SIZE, getPosZ() + (rand.nextDouble() - 0.5) * TIME_FIELD_SIZE, 0.0, 0.0, 0.0);
+                world.addParticle(ParticleTypes.ENTITY_EFFECT, getPosX() + (rand.nextDouble() - 0.5) * TIME_FIELD_SIZE, getPosY() + (rand.nextDouble() - 0.5) * TIME_FIELD_SIZE, getPosZ() + (rand.nextDouble() - 0.5) * TIME_FIELD_SIZE, 0.0, 0.0, 0.0);
             }
         } else {
             remove();
