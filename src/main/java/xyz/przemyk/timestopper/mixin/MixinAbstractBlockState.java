01c561ef377237c058d116212f534fec07019c21
Przemek <przemek@przemyk.xyz>
2020-11-23 17:59:12 +0100

    updated to newest version

--- src/main/java/xyz/przemyk/timestopper/mixin/MixinAbstractBlockState.java
+++ src/main/java/xyz/przemyk/timestopper/mixin/MixinAbstractBlockState.java
@@ -20,23 +20,19 @@ import java.util.List;
 @Mixin(AbstractBlock.AbstractBlockState.class)
 public abstract class MixinAbstractBlockState extends StateHolder<Block, BlockState> {
 
-    protected MixinAbstractBlockState(Block p_i231879_1_, ImmutableMap<Property<?>, Comparable<?>> p_i231879_2_, MapCodec<BlockState> p_i231879_3_) {
-        super(p_i231879_1_, p_i231879_2_, p_i231879_3_);
+    protected MixinAbstractBlockState(Block block, ImmutableMap<Property<?>, Comparable<?>> propertyValueMap, MapCodec<BlockState> stateCodec) {
+        super(block, propertyValueMap, stateCodec);
     }
 
-    @Shadow
-    public Block getBlock() {
-        return this.field_235892_c_;
-    }
+    @Shadow protected abstract BlockState getSelf();
 
-    @Shadow
-    protected abstract BlockState func_230340_p_();
+    @Shadow public abstract Block getBlock();
 
     //TODO: work with capability
     public void neighborChanged(World worldIn, BlockPos posIn, Block blockIn, BlockPos fromPosIn, boolean isMoving) {
         List<ActiveTimeStopperEntity> activeTimeStoppers = worldIn.getEntitiesWithinAABB(ActiveTimeStopperEntity.class, TimeStopperMod.scan.offset(posIn));
         if (activeTimeStoppers.isEmpty()) {
-            this.getBlock().neighborChanged(this.func_230340_p_(), worldIn, posIn, blockIn, fromPosIn, isMoving);
+            this.getBlock().neighborChanged(this.getSelf(), worldIn, posIn, blockIn, fromPosIn, isMoving);
             return;
         }
 
