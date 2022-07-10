29e00a8e8850e8f0871f301a4ab861d0255cdd72
Przemyk <przemek@przemyk.xyz>
2022-07-10 11:13:18 +0200

    time speed change now works with block entities

--- src/main/java/xyz/przemyk/timestopper/TimeStopperMod.java
+++ src/main/java/xyz/przemyk/timestopper/TimeStopperMod.java
@@ -5,6 +5,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.TickingBlockEntity;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.fml.common.Mod;
@@ -37,18 +38,6 @@ public class TimeStopperMod {
         }
     };
 
-    public static boolean canUpdate(Vec3 position, Level world) {
-        AABB toScan = scan.move(position);
-
-        for (Player playerEntity : world.getEntitiesOfClass(Player.class, toScan)) {
-            if (playerEntity.getCapability(TimeStateHandlerProvider.TIME_STATE_CAP).map(h -> h.timeState == TimeState.STOPPED).orElse(false)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     public static boolean canUpdateEntity(Entity entity) {
         AABB toScan = scan.move(entity.position());
         if (entity.getCapability(TimeStateHandlerProvider.TIME_STATE_CAP).map(h -> h.timeState == TimeState.STOPPED).orElse(false)) {
@@ -74,40 +63,62 @@ public class TimeStopperMod {
         }
     }
 
+    public static void updateBlockEntity(TickingBlockEntity tickingBlockEntity, Level level) {
+        TimeState timeState = TimeState.NORMAL;
+
+        for (Player playerEntity : level.getEntitiesOfClass(Player.class, scan.move(tickingBlockEntity.getPos()))) {
+            TimeState newTimeState = playerEntity.getCapability(TimeStateHandlerProvider.TIME_STATE_CAP).map(handler -> handler.timeState).orElse(TimeState.NORMAL);
+            if (newTimeState.ordinal() > timeState.ordinal()) {
+                timeState = newTimeState;
+            }
+        }
+
+        switch (timeState) {
+            case NORMAL:
+                tickingBlockEntity.tick();
+                break;
+            case SLOW:
+                if (level.getGameTime() % 2 == 0) {
+                    tickingBlockEntity.tick();
+                }
+                break;
+            case FAST:
+                tickingBlockEntity.tick();
+                tickingBlockEntity.tick();
+        }
+    }
+
     public static void updateEntity(Entity entity) {
-        if (entity.canUpdate()) {
+        TimeState timeState = TimeState.NORMAL;
 
-            TimeState timeState = TimeState.NORMAL;
-
-            for (Player playerEntity : entity.level.getEntitiesOfClass(Player.class, scan.move(entity.position()))) {
-                if (entity != playerEntity) {
-                    TimeState newTimeState = playerEntity.getCapability(TimeStateHandlerProvider.TIME_STATE_CAP).map(handler -> {
-                        if (handler.timeState == TimeState.STOPPED &&
-                                entity.getCapability(TimeStateHandlerProvider.TIME_STATE_CAP).map(handlerEntity -> handlerEntity.timeState == TimeState.STOPPED).orElse(false)) {
-                            return TimeState.NORMAL;
-                        } else {
-                            return handler.timeState;
-                        }
-                    }).orElse(TimeState.NORMAL);
-                    if (newTimeState.ordinal() > timeState.ordinal()) {
-                        timeState = newTimeState;
+        for (Player playerEntity : entity.level.getEntitiesOfClass(Player.class, scan.move(entity.position()))) {
+            if (entity != playerEntity) {
+                TimeState newTimeState = playerEntity.getCapability(TimeStateHandlerProvider.TIME_STATE_CAP).map(handler -> {
+                    if (handler.timeState == TimeState.STOPPED &&
+                            entity.getCapability(TimeStateHandlerProvider.TIME_STATE_CAP).map(handlerEntity -> handlerEntity.timeState == TimeState.STOPPED).orElse(false)) {
+                        return TimeState.NORMAL;
+                    } else {
+                        return handler.timeState;
                     }
+                }).orElse(TimeState.NORMAL);
+                if (newTimeState.ordinal() > timeState.ordinal()) {
+                    timeState = newTimeState;
                 }
             }
+        }
 
-            switch (timeState) {
-                case NORMAL:
+        switch (timeState) {
+            case NORMAL:
+                entity.tick();
+                break;
+            case SLOW:
+                if (entity.level.getGameTime() % 2 == 0) {
                     entity.tick();
-                    break;
-                case SLOW:
-                    if (entity.level.getGameTime() % 2 == 0) {
-                        entity.tick();
-                    }
-                    break;
-                case FAST:
-                    entity.tick();
-                    entity.tick();
-            }
+                }
+                break;
+            case FAST:
+                entity.tick();
+                entity.tick();
         }
     }
 }
---
+++ src/main/java/xyz/przemyk/timestopper/mixin/MixinLevel.java
@@ -0,0 +1,70 @@
+package xyz.przemyk.timestopper.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.TickingBlockEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import xyz.przemyk.timestopper.TimeStopperMod;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@SuppressWarnings("unused")
+@Mixin(Level.class)
+public abstract class MixinLevel extends net.minecraftforge.common.capabilities.CapabilityProvider<Level> implements LevelAccessor, AutoCloseable, net.minecraftforge.common.extensions.IForgeLevel {
+    @Shadow public abstract ProfilerFiller getProfiler();
+
+    @Shadow @Final private ArrayList<BlockEntity> pendingFreshBlockEntities;
+
+    @Shadow @Final private ArrayList<BlockEntity> freshBlockEntities;
+
+    @Shadow private boolean tickingBlockEntities;
+
+    @Shadow @Final private List<TickingBlockEntity> pendingBlockEntityTickers;
+
+    @Shadow @Final protected List<TickingBlockEntity> blockEntityTickers;
+
+    @Shadow public abstract boolean shouldTickBlocksAt(BlockPos p_220394_);
+
+    protected MixinLevel(Class<Level> baseClass) {
+        super(baseClass);
+    }
+
+    protected void tickBlockEntities() {
+        ProfilerFiller profilerfiller = this.getProfiler();
+        profilerfiller.push("blockEntities");
+        if (!this.pendingFreshBlockEntities.isEmpty()) {
+            this.freshBlockEntities.addAll(this.pendingFreshBlockEntities);
+            this.pendingFreshBlockEntities.clear();
+        }
+        this.tickingBlockEntities = true;
+        if (!this.freshBlockEntities.isEmpty()) {
+            this.freshBlockEntities.forEach(BlockEntity::onLoad);
+            this.freshBlockEntities.clear();
+        }
+        if (!this.pendingBlockEntityTickers.isEmpty()) {
+            this.blockEntityTickers.addAll(this.pendingBlockEntityTickers);
+            this.pendingBlockEntityTickers.clear();
+        }
+
+        Iterator<TickingBlockEntity> iterator = this.blockEntityTickers.iterator();
+
+        while(iterator.hasNext()) {
+            TickingBlockEntity tickingblockentity = iterator.next();
+            if (tickingblockentity.isRemoved()) {
+                iterator.remove();
+            } else if (this.shouldTickBlocksAt(tickingblockentity.getPos())) {
+                TimeStopperMod.updateBlockEntity(tickingblockentity, (Level) (Object) this);
+            }
+        }
+
+        this.tickingBlockEntities = false;
+        profilerfiller.pop();
+    }
+}
